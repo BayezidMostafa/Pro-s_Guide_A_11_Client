de4071ab921091e1fd21645a3378a3b47aa11e5c
Bayezid Mostafa <hiperx1@gmail.com>
2022-11-09 23:39:35 +0600

    Review Delete Method Implemented In My Review Component

--- src/Pages/MyReviews/MyReviews.js
+++ src/Pages/MyReviews/MyReviews.js
@@ -3,20 +3,48 @@ import { AuthContext } from '../../Context/AuthProvider';
 import Review from './Review';
 
 const MyReviews = () => {
-    const {user} = useContext(AuthContext);
+    const { user } = useContext(AuthContext);
     const [reviews, setReviews] = useState([])
     useEffect(() => {
         fetch(`http://localhost:5000/myReviews?email=${user?.email}`)
-        .then(res => res.json())
-        .then(data => {
-            setReviews(data)
-            console.log(data);
-        })
+            .then(res => res.json())
+            .then(data => {
+                setReviews(data)
+                console.log(data);
+            })
     }, [user?.email])
+
+    const handleDelete = _id => {
+        const confirmation = window.confirm("Are you sure? You Want To Delete This Review!")
+        if (confirmation) {
+            fetch(`http://localhost:5000/myReviews/${_id}`, {
+                method: 'DELETE',
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deleteCount > 0) {
+                        alert('Review Deleted Successfully')
+                        const remainingReview = reviews.filter(review => review._id !== _id)
+                        setReviews(remainingReview);
+                    }
+                })
+
+        }
+    }
+
     return (
-        <div className='container mx-auto mt-10'>
+        <div className='container mx-auto mt-10 min-h-[70.5vh]'>
             {
-                reviews.map(review => <Review key={review._id} review={review}/>)
+                reviews.length === 0 ?
+                <>
+                    <p className='text-3xl font-semibold text-center'>No Review Found. Please Add Some Review!</p>
+                </>
+            :
+            <>
+                {
+                    reviews.map(review => <Review key={review._id} review={review} handleDelete={handleDelete} />)
+                }
+            </>
             }
         </div>
     );
--- src/Pages/MyReviews/Review.js
+++ src/Pages/MyReviews/Review.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { FaTimes } from "react-icons/fa";
 
 
-const Review = ({ review }) => {
-    const { picture, name, email, review_comment } = review;
+const Review = ({ review, handleDelete }) => {
+    const { picture, _id, name, email, review_comment } = review;
     return (
         <div className="relative grid grid-cols-1 gap-4 p-4 mb-8 border rounded-lg bg-white shadow-lg">
             <div className="relative flex gap-4">
@@ -11,7 +11,7 @@ const Review = ({ review }) => {
                 <div className="flex flex-col w-full">
                     <div className="md:flex flex-row justify-between">
                         <p className="relative text-xl whitespace-nowrap truncate overflow-hidden">{name}</p>
-                        <FaTimes className='text-red-400 text-2xl cursor-pointer hover:text-red-700 duration-150 bg-gray-200 rounded bg:shadow-md shadow-black' />
+                        <FaTimes onClick={()=>handleDelete(_id)} className='text-red-400 text-2xl cursor-pointer hover:text-red-700 duration-150 bg-gray-200 rounded bg:shadow-md shadow-black' />
                         {/* <p>Rating: {rating}</p> */}
                     </div>
                     <p className="text-gray-400 text-sm">{email}</p>
